23d7b9300683fa450c7c2484a41ad993d14297a7
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-11 10:15:33 -0800

    created supplier model specs

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "is valid with a name, a representative, an email, and an address" do
+    supplier = Supplier.new(
+      name: "test supplier",
+      representative: "test rep",
+      email: "test@example.com",
+      address: "1234 test address"
+    )
+    expect(supplier).to be_valid
+  end
+
+  it "is invalid without a name" do
+    supplier = Supplier.new(name: nil)
+    supplier.valid?
+    expect(supplier.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a representative" do
+    supplier = Supplier.new(reqresentative: nil)
+    supplier.valid?
+    expect(supplier.errors[:reqresentative]).to include("can't be blank")
+  end
+
+  it "is invalid without an email" do
+    supplier = Supplier.new(email: nil)
+    supplier.valid?
+    expect(supplier.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicated email address" do
+    Supplier.create(
+      name: "test",
+      reqresentative: "test rep",
+      email: "test@example.com"
+    )
+    supplier = Supplier.new(
+      name: "test2",
+      representative: "test rep",
+      email: "test@example.com"
+    )
+    supplier.valid?
+    expect(supplier.errors[:email]).to include("has already been taken")
+  end
+
 end
