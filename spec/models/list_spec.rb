e930ab5480cc4c25f5b6e15410cd2804ba083b61
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-11 23:31:24 -0800

    craete list validation specs

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,13 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "is valid with an order_id, an item_id, an amount, a list price" do
-    order = Order.new(
-      order_number: "1001",
-      # TODO: リレーションによる外部キー制約のテストにもパスするように書く
-      )
-    list = order.build(
-      # TODO: OrderとItemに紐付くように書く
+
+  before do
+    @customer = Customer.create(
+      name: 'test',
+      email: 'test@example.com',
+      address: '1234 Abc Street'
     )
+    @item = Item.create(
+      name: 'test',
+      price: 15,
+      stock: 20
+    )
+    @order = @customer.orders.build
+    @list = @order.lists.build
+  end
+
+  it "is valid with an order_id, an item_id, an amount, a list price" do
+    @list.item_id = @item.id
+    @list.amount = 5
+    @list.list_price = 15
+    expect(@list).to be_valid
+  end
+
+  it "is invalid without item_id" do
+    @list.item_id = nil
+    @list.valid?
+    expect(@list.errors[:item_id]).to include("Item can't be blank")
+  end
+
+  it "is invalid without amount" do
+    @list.amount = nil
+    @list.valid?
+    expect(@list.errors[:amount]).to include("can't be blank")
+  end
+
+  it "is invalid without list price" do
+    @list.list_price = nil
+    @list.valid?
+    expect(@list.errors[:list_price]).to include("can't be blank")
   end
 end
