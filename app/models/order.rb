6ab4079da6ff7300eb9ae2bbf993a80b929d865a
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-11 23:17:37 -0800

    pass all the orders validation specs

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  validates :order_number, presence: true
+  validates :order_date, presence: true
+
   belongs_to :customer, optional: true
   has_many :lists, dependent: :destroy
 
