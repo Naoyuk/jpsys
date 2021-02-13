5e89c2aa3ff0246464e474fb21af4ab5d8ab0e95
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-13 12:13:08 -0800

    complete user validations and their specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
