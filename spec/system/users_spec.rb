00ace30a93c96fe7d850440ee8a4d5c820e3f566
Naoyuki Ishida <39.ishida@gmail.com>
2021-02-15 17:13:20 -0800

    create sign in system spec and implement sign in feature

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,5 +5,33 @@ RSpec.describe "Users", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario "successfully signs up and got email and redirect to root path" do
+
+    visit new_user_path
+    click_link "Sign up"
+
+    expect {
+      fill_in "Name", with: "test"
+      fill_in "Email", with: "test@example.com"
+      fill_in "Password", with: "foobar"
+      fill_in "Password Confirmation", with: "foobar"
+      click_button "Sign up"
+    }.to change(User, :count).by(1)
+
+  end
+
+  scenario "fail signs up with invalid information and redirect to new user path" do
+
+    visit new_user_path
+    click_link "Sign up"
+
+    expect {
+      fill_in "Name", with: "test"
+      fill_in "Email", with: "test@example.com"
+      fill_in "Password", with: "foobar"
+      fill_in "Password Confirmation", with: "barbaz"
+      click_button "Sign up"
+    }.to change(User, :count).by(0)
+
+  end
 end
